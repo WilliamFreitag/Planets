6858bf4f0901d813df517ff2ca293fba12885dcb
William Freitag <william.h.freitag@gmail.com>
2020-08-22 12:26:12 -0700

    added merge on collision functionality

--- planets.js
+++ planets.js
@@ -19,7 +19,7 @@ universe.addEventListener('mousedown', (e) => {
   lastPlanetMade = planets.length;
   planets[planets.length] = {
     color: getRandomColor(),
-    radius: Math.floor(Math.random()*50+1),
+    radius: Math.floor(Math.random()*25+1),
     acceleration: [0,0],
     velocity: [0,0],
     position: [e.clientX,e.clientY],
@@ -30,7 +30,7 @@ universe.addEventListener('mousedown', (e) => {
 
 universe.addEventListener('mouseup',(e) => {
   var tip = [e.clientX,e.clientY];
-  var v = [(tip[0]-planets[lastPlanetMade].position[0])/100,(tip[1]-planets[lastPlanetMade].position[1])/100];
+  var v = [(tip[0]-planets[lastPlanetMade].position[0])/100,(tip[1]-planets[lastPlanetMade].position[1])/150];
   planets[lastPlanetMade].velocity = v;
 });
 setUpExample();
@@ -56,24 +56,32 @@ function setUpExample(){
     color: "#00CCFF",
     radius:2,
     acceleration: [0,0],
-    velocity: [0,-8],
-    position: [250,500],
+    velocity: [0,-12],
+    position: [1000,500],
     force: [0,0]
   };
   planets[2] = {
-    color: "#CC00CC",
-    radius:5,
+    color: "#FFCC00",
+    radius:3,
     acceleration: [0,0],
-    velocity: [0,6],
-    position: [1400,500],
+    velocity: [0,10],
+    position: [1100,500],
     force: [0,0]
   };
   planets[3] = {
     color: "#FFCC00",
-    radius:3,
+    radius:10,
     acceleration: [0,0],
-    velocity: [12,0],
-    position: [750,750],
+    velocity: [0,-9],
+    position: [1200,500],
+    force: [0,0]
+  };
+  planets[4] = {
+    color: "#FFCC00",
+    radius:12,
+    acceleration: [0,0],
+    velocity: [0,8],
+    position: [1300,500],
     force: [0,0]
   };
   render();
@@ -101,24 +109,40 @@ function loop(){
 
 function tick(){
   console.log("tick");
-  console.log(planets);
   for(var i = 0; i < planets.length; i++){
+    if(planets[i] === undefined) continue;
     var m1 = 3.1415926535*Math.pow(planets[i].radius,2);
     planets[i].force[0] = 0;
     planets[i].force[1] = 0;
     for(var j = 0; j < planets.length; j++){
-      if(planets[i] === planets[j]) continue;
+      if(planets[i] === planets[j]||planets[i] === undefined || planets[j] === undefined) continue;
       var r = [planets[j].position[0]-planets[i].position[0],planets[j].position[1]-planets[i].position[1]];
-      while(r[0] === 0) r[0] = (Math.random()-0.5)*5; //fake physics to avoid 0 distance when objects are perfectly on top of each other
-      while(r[1] === 0) r[1] = (Math.random()-0.5)*5; //fake physics to avoid 0 distance when objects are perfectly on top of each other
       var rMagnitude = Math.pow(Math.pow(r[0],2)+Math.pow(r[1],2),0.5);
+      if(rMagnitude <= planets[j].radius+planets[i].radius){
+        planets[i].velocity[0] *= (3.1415926535*Math.pow(planets[i].radius,2));
+        planets[i].velocity[1] *= (3.1415926535*Math.pow(planets[i].radius,2));
+        planets[i].velocity[0] += (planets[j].velocity[0]*(3.1415926535*Math.pow(planets[j].radius,2)));
+        planets[i].velocity[1] += (planets[j].velocity[1]*(3.1415926535*Math.pow(planets[j].radius,2)));
+        planets[i].radius += planets[j].radius;
+        planets[i].velocity[0] /= (3.1415926535*Math.pow(planets[i].radius,2));
+        planets[i].velocity[1] /= (3.1415926535*Math.pow(planets[i].radius,2));
+        if(lastPlanetMade===j)lastPlanetMade=i;
+        planets.splice(j,1);
+        continue;
+      }
+    }
+    for(var j = 0; j < planets.length; j++){
+      if(planets[i] === planets[j]||planets[i] === undefined || planets[j] === undefined) continue;
+      var r = [planets[j].position[0]-planets[i].position[0],planets[j].position[1]-planets[i].position[1]];
+      var rMagnitude = Math.pow(Math.pow(r[0],2)+Math.pow(r[1],2),0.5);
+      if(rMagnitude <= planets[j].radius+planets[i].radius)continue;
       var rUnit = [r[0]/rMagnitude,r[1]/rMagnitude];
-      if(rMagnitude < 100)rMagnitude = 100; //fake physics to avoid super wild slingshots when objects phase through each other
       var m2 = 3.14*Math.pow(planets[j].radius,2);
       var forceMagnitude = (g*m1*m2)/Math.pow(rMagnitude,2);
       planets[i].force[0] += rUnit[0]*forceMagnitude;
       planets[i].force[1] += rUnit[1]*forceMagnitude;
     }
+    if(planets[i] === undefined) continue;
     planets[i].acceleration = [planets[i].force[0]/m1,planets[i].force[1]/m1];
     planets[i].velocity[0] += planets[i].acceleration[0];
     planets[i].velocity[1] += planets[i].acceleration[1];
